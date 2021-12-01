68e503015b30bed7c88aa9328fec6cfefb57507b
Lea Fairbanks <lea.rae.fairbanks@gmail.com>
2021-12-01 09:45:48 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ fn main() {
     let contents = fs::read_to_string(part1_file)
         .expect("Something went wrong reading the file");
 
-    let mut lines1 = contents.lines();
-    part1(lines1);
+    let lines1 = contents.lines();
+    println!("part 1: {}", part1(lines1));
 
     let part2_file = "input2.txt";
     let contents = fs::read_to_string(part2_file)
         .expect("Something went wrong reading the file");
 
-    let mut lines2 = contents.lines();
-    part2(lines2);
+    let lines2 = contents.lines();
+    println!("part 2: {}", part2(lines2));
 }
 
 fn part1(mut lines: Lines) -> u32 {
@@ -26,18 +26,15 @@ fn part1(mut lines: Lines) -> u32 {
     loop {
         let next = lines.next();
         if next.is_none() {
-            println!("got none");
             break;
         }
-        let num = next.unwrap().parse::<u32>().unwrap();;
+        let num = next.unwrap().parse::<u32>().unwrap();
         if num > prev {
-            println!("num {} gt prv {}", num, prev);
             gt_count += 1;
         }
         prev = num;
     }
 
-    println!("gt count: {}", gt_count);
     gt_count
 }
 
@@ -47,28 +44,22 @@ fn part2(mut lines: Lines) -> u32 {
     let mut third = lines.next().unwrap().parse::<u32>().unwrap();
     let mut gt_count = 0;
 
-    let mut window1 = 0;
-    let mut window2 = 0;
-
     loop {
         let next = lines.next();
         if next.is_none() {
-            println!("got none");
             break;
         }
         let fourth = next.unwrap().parse::<u32>().unwrap();
-        window1 = first + second + third;
-        window2 = second + third + fourth;
+        let window1 = first + second + third;
+        let window2 = second + third + fourth;
         if window2 > window1 {
             gt_count += 1;
         }
-        window1 = window2;
         first = second;
         second = third;
         third = fourth;
     }
 
-    println!("gt count: {}", gt_count);
     gt_count
 }
 
