a335da79a619ddbd445de0d087557fdaf24a3c03
Lea Fairbanks <lea.rae.fairbanks@gmail.com>
2021-12-02 13:33:24 -0800

    setup for day 2

---
+++ src/day1/mod.rs
@@ -0,0 +1,94 @@
+use std::fs;
+use std::str::Lines;
+
+
+pub fn run() {
+    let part1_file = "src/day1/input.txt";
+
+    let contents = fs::read_to_string(part1_file)
+        .expect("Something went wrong reading the file");
+
+    let lines1 = contents.lines();
+    println!("part 1: {}", part1(lines1));
+
+    let part2_file = "src/day1/input2.txt";
+    let contents = fs::read_to_string(part2_file)
+        .expect("Something went wrong reading the file");
+
+    let lines2 = contents.lines();
+    println!("part 2: {}", part2(lines2));
+}
+
+fn part1(mut lines: Lines) -> u32 {
+    let mut prev = lines.next().unwrap().parse::<u32>().unwrap();
+
+    let mut gt_count = 0;
+
+    loop {
+        let next = lines.next();
+        if next.is_none() {
+            break;
+        }
+        let num = next.unwrap().parse::<u32>().unwrap();
+        if num > prev {
+            gt_count += 1;
+        }
+        prev = num;
+    }
+
+    gt_count
+}
+
+fn part2(mut lines: Lines) -> u32 {
+    let mut first = lines.next().unwrap().parse::<u32>().unwrap();
+    let mut second = lines.next().unwrap().parse::<u32>().unwrap();
+    let mut third = lines.next().unwrap().parse::<u32>().unwrap();
+    let mut gt_count = 0;
+
+    loop {
+        let next = lines.next();
+        if next.is_none() {
+            break;
+        }
+        let fourth = next.unwrap().parse::<u32>().unwrap();
+        let window1 = first + second + third;
+        let window2 = second + third + fourth;
+        if window2 > window1 {
+            gt_count += 1;
+        }
+        first = second;
+        second = third;
+        third = fourth;
+    }
+
+    gt_count
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::day1::*;
+
+    #[test]
+    fn part1_works() {
+        let filename = "src/day1/test.txt";
+
+        let contents = fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+
+        let lines = contents.lines();
+        assert_eq!(part1(lines.clone()), 7);
+    }
+
+    #[test]
+    fn part2_works() {
+        let filename = "src/day1/test2.txt";
+
+        let contents = fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+
+        let lines = contents.lines();
+        assert_eq!(part2(lines.clone()), 5);
+    }
+}
